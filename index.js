a56c1ddf54c1b6684936a6e65affbe3578e88c57
Dong Don <don.dong@volvo.com>
2020-04-13 17:29:52 +0800

    []

---
+++ index.js
@@ -0,0 +1,5 @@
+/*https://www.jianshu.com/p/e8b33674b942*/
+
+exports.printMSG = function() {
+    console.log('This message is from my npm module.');
+}
---
+++ js/Dmixin.js
@@ -0,0 +1,55 @@
+
+let dmixin = {
+    superRef: null,
+    data() {
+        return {
+            demo: "demo",
+        }
+    },
+    /*props conflicts with ant vue,so use this.$el replace it.*/
+    /*
+    props: {
+        superRef: {
+            type: String,
+        },
+    },*/
+    computed: {},
+    methods: {
+        doMethod(superRef, method, data) {
+            if (this.superRef!=null) {
+                const event = superRef + "." + method;
+                this.$listen.$emit(event, data);
+            }
+        }
+    },
+    destroyed() {
+        if (this.superRef!=null) {
+            for (let key in this.$options.methods) {
+                const m = key;
+                const allMethod = this.superRef + "." + m
+                this.$listen.$off(allMethod);
+                // console.log("destroy"+allMethod )
+            }
+        }
+    },
+    mounted() {
+        /*props conflicts with ant vue,so use this.$el replace it.*/
+        try {
+            this.superRef =this.$el.getAttribute("superRef");
+        }catch (e) {
+
+        }
+        // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        if (this.superRef!=null) {
+            for (let key in this.$options.methods) {
+                const m = key;
+                const allMethod = this.superRef + "." + m
+                // console.log("create listen"+allMethod)
+                this.$listen.$off(allMethod);
+                this.$listen.$on(allMethod, this.$options.methods[key])
+            }
+        }
+    }
+}
+export {dmixin};
+
